08adcf84807d1563d59d428ea7361b135c931b40
Zhang Zeyu <997130886@qq.com>
2022-06-24 00:51:28 +0800

    format code

--- app/src/main/java/com/example/pixelboom/MainActivity.java
+++ app/src/main/java/com/example/pixelboom/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.pixelboom;
 
 import android.os.Bundle;
+
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 import com.example.pixelboom.databinding.ActivityMainBinding;
 
 import android.view.Menu;
@@ -16,14 +21,14 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-private ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
@@ -39,7 +44,8 @@ private ActivityMainBinding binding;
             }
         });
     }
-@Override
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
